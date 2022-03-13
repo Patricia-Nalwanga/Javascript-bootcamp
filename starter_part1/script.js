6c97fb95fdd8223d7ae78a5ef53c245709582fae
Patricia_Nalwanga <pmubiru2020@gmail.com>
2022-03-13 18:51:46 +0300

    add index and script files

---
+++ starter_part1/script.js
@@ -0,0 +1,43 @@
+// let js = "amazing";
+// console.log(40 + 23 + 8 - 10);
+
+// let firstName = "Patricia";
+// console.log(firstName);
+
+// let PI = 3.14;
+// console.log(typeof PI);
+
+// //example of dynamic typing.
+// let javascriptIsFun = true;
+// console.log(typeof javascriptIsFun);
+
+// javascriptIsFun = "HEY";
+// console.log(typeof javascriptIsFun);
+
+// Coding challenge 1
+
+// for data1
+const firstMassMark = 78;
+const firstHeightMark = 1.69;
+const firstBMIMark = firstMassMark / (firstHeightMark * firstHeightMark);
+console.log(firstBMIMark);
+
+const firstMassJohn = 92;
+const firstHeightJohn = 1.95;
+const firstBMIJohn = firstMassJohn / (firstHeightJohn * firstHeightJohn);
+console.log(firstBMIJohn);
+
+const markHigherBMI = firstBMIMark > firstBMIJohn;
+console.log(markHigherBMI);
+
+// for data2
+const secondMassMark = 95;
+const secondHeightMark = 1.88;
+const secondBMIMark = secondMassMark / (secondHeightMark * secondHeightMark);
+console.log(secondBMIMark);
+
+const secondMassJohn = 85;
+const secondHeightJohn = 1.76;
+const secondBMIJohn = secondMassJohn / (secondHeightJohn * secondHeightJohn);
+console.log(secondBMIJohn);
+
