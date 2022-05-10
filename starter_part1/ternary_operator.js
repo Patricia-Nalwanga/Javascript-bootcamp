87c17f7fd1c7e05f64cdbe5659847389ba3a29a0
Patricia_Nalwanga <pmubiru2020@gmail.com>
2022-05-10 12:36:47 +0300

    modified ternary_operator.js file

--- starter_part1/ternary_operator.js
+++ starter_part1/ternary_operator.js
@@ -8,7 +8,7 @@
 //     bill*0.2;
 // }
 const bill = 430;
-const tip = bill > 50 && bill < 300 ? bill * 0.15 : bill * 0.2;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
 console.log(
   `The bill was ${bill},the tip was ${tip} and the total value was ${
