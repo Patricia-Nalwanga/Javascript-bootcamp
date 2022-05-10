24376ce9b61db9830b3b4c796d72a4bfad7eb73b
Patricia_Nalwanga <pmubiru2020@gmail.com>
2022-05-10 12:34:23 +0300

    completed coding challenge2 of part2.

--- jsFundamentals_part2/array.js
+++ jsFundamentals_part2/array.js
@@ -59,3 +59,18 @@ const calcTip = function (bill) {
   }
 };
 calcTip(100);
+
+// task 2
+const bills = [125, 555, 44];
+
+// task3
+const tip1 = calcTip(bills[0]);
+const tip2 = calcTip(bills[1]);
+console.log(tip1, tip2);
+
+const tips = [tip1, tip2, calcTip(bills[bills.length - 1])];
+console.log(tips);
+
+// task4
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(totals);
