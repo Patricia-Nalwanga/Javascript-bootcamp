5426969f941c2cf48a4db6da103358d2825cb35f
Patricia_Nalwanga <pmubiru2020@gmail.com>
2022-05-22 12:10:19 +0300

    literal syntax of an object

---
+++ jsFundamentals_part2/object.js
@@ -0,0 +1,9 @@
+// literal syntax
+const profile = {
+  firstName: "Patricia",
+  lastName: "Nalwanga",
+  age: 2022 - 2000,
+  job: "software developer",
+  friends: ["Peter", "Jane", "Steven"],
+};
+console.log(typeof profile);
