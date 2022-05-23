8b54d4daf849b1ac91a8dcafe3c546d7697e8aaa
Patricia_Nalwanga <pmubiru2020@gmail.com>
2022-05-24 02:01:32 +0300

    super proud of me

--- jsFundamentals_part2/object.js
+++ jsFundamentals_part2/object.js
@@ -1,4 +1,4 @@
-// "use strict";
+"use strict";
 // literal syntax
 const profile = {
   firstName: "Patricia",
@@ -31,3 +31,25 @@ const bestie = profile.friends;
 console.log(
   `${profile.firstName} has ${profile.friends.length} friends, and her bestfriend is ${bestie[0]}`
 );
+
+// challenge
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Mubiru",
+  birthYear: "2000",
+  job: "teacher",
+  friends: ["Cynthia", "Edward", "Brian"],
+  hasDriversLicense: false,
+
+  calcAge: function () {
+    this.age = 2022 - this.birthYear;
+    return this.age;
+  },
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()} year old ${
+      this.job
+    } and has ${this.hasDriversLicense ? "a" : "no"} drivers license.`;
+  },
+};
+
+console.log(jonas.getSummary());
