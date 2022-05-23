6d9e451d439a1de93c6a309b995497db4b1114d0
Patricia_Nalwanga <pmubiru2020@gmail.com>
2022-05-23 12:06:23 +0300

    object dot and bracket notation

--- jsFundamentals_part2/object.js
+++ jsFundamentals_part2/object.js
@@ -1,3 +1,4 @@
+// "use strict";
 // literal syntax
 const profile = {
   firstName: "Patricia",
@@ -7,3 +8,26 @@ const profile = {
   friends: ["Peter", "Jane", "Steven"],
 };
 console.log(typeof profile);
+
+/*retrieving data from an object
+1.using dot notation*/
+console.log(profile.friends);
+
+//using bracket notation
+console.log(profile["age"]);
+
+const interestedIn = prompt(
+  "What do you want to know about Nalwanga? Choose between job and friends"
+);
+console.log(interestedIn);
+
+if (profile[interestedIn]) {
+  console.log(profile[interestedIn]);
+} else {
+  console.log("Wrong choice! Please choose between job and friends");
+}
+
+const bestie = profile.friends;
+console.log(
+  `${profile.firstName} has ${profile.friends.length} friends, and her bestfriend is ${bestie[0]}`
+);
