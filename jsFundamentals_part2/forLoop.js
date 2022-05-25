ac6a8f8c024d0fc4d2d1fe700eb8af73d179bd1a
Patricia_Nalwanga <pmubiru2020@gmail.com>
2022-05-25 16:46:58 +0300

    reading and filling an array

--- jsFundamentals_part2/forLoop.js
+++ jsFundamentals_part2/forLoop.js
@@ -3,3 +3,25 @@
 for (let rep = 1; rep <= 10; rep++) {
   console.log(`Lifting weights repetition ${rep}`);
 }
+
+// using a for loop to iterate through an array
+const jonasArray = [
+  "Jonas",
+  "Ssendi",
+  2022 - 2000,
+  "teacher",
+  ["Peter", "Jane", "Steven"],
+  true,
+];
+console.log(jonasArray.length);
+
+const types = [];
+for (let i = 0; i < jonasArray.length; i++) {
+  console.log(jonasArray[i]);
+
+  //   filling an array
+  types.push(typeof jonasArray[i]);
+  //   or types[i]=typeof jonasArray[i];
+}
+
+console.log(types);
