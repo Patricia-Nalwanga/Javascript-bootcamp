d445d0d609ca555f3598bb2f9661189dfc6a16b0
Patricia_Nalwanga <pmubiru2020@gmail.com>
2022-05-28 00:27:41 +0300

    looping arrays and break & continue statement

--- jsFundamentals_part2/forLoop.js
+++ jsFundamentals_part2/forLoop.js
@@ -25,3 +25,27 @@ for (let i = 0; i < jonasArray.length; i++) {
 }
 
 console.log(types);
+
+// example 2
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+for (let i = 0; i < years.length; i++) {
+  ages.push(2022 - years[i]);
+}
+console.log(ages);
+
+// continue and break
+console.log("---ONLY STRINGS---");
+for (let i = 0; i < jonasArray.length; i++) {
+  if (typeof jonasArray[i] !== "string") continue;
+
+  console.log(jonasArray[i], typeof jonasArray[i]);
+}
+
+console.log("---BREAK WITH NUMBER ---");
+for (let i = 0; i < jonasArray.length; i++) {
+  if (typeof jonasArray[i] === "number") break;
+
+  console.log(jonasArray[i], typeof jonasArray[i]);
+}
