b65ea4deb020835690fbf80607cf20a522519644
Patricia_Nalwanga <pmubiru2020@gmail.com>
2022-06-22 22:41:11 +0300

    coding challenge completed

--- developer_skills/debug.js
+++ developer_skills/debug.js
@@ -1,15 +1,21 @@
 "use strict";
 
 // coding challenge
+
 const printForecast = function (arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 1; j <= arr.length; j++) {
-      let day = j;
-      const temps = `.....${arr[i]} in ${day} days`;
-      console.log(temps);
-      break;
-    }
+  let day = 0;
+let temp;
+let thermometerReading = '';
+
+  for(let i=0; i<arr.length; i++){
+    temp = arr[i];
+    day+=1;
+    thermometerReading = thermometerReading + `...${temp} in ${day} days`;
+     
+    
   }
+  console.log(thermometerReading);
+
 };
 
 printForecast([17, 21, 23]);
